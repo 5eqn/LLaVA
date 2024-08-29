2f94cc0220b8e2fe6c3638e2ad654aa3bb55df60
5eqn <491100866@qq.com>
2024-08-29 18:14:00 +0800

    update finetune docs

---
+++ REPRODUCE.md
@@ -0,0 +1,12 @@
+## Evaluate Flickr30k
+
+1. Download Flickr30k dataset with automatic download tool in LAVIS repo
+2. Run `python convert/flickr30k.py`
+3. TODO generate test result with correct format
+
+## Finetune with Flickr30k
+
+1. Download Flickr30k dataset with automatic download tool in LAVIS repo
+2. Run `python convert/flickr30k.py`
+3. Get pretrained projector weight with `git clone https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5` at home directory
+4. Run `bash scripts/v1_5/finetune-flickr30k.sh`
---
+++ convert/convert-flickr30k.sh
@@ -0,0 +1,13 @@
+# Convert Flickr30k dataset to compatible format for train dataset
+python convert/flickr30k.py --input_file='../.cache/lavis/flickr30k/annotations/train.json' --output_file='./playground/data/train.json'
+# python convert/flickr30k.py --input_file='../.cache/lavis/flickr30k/annotations/val.json' --output_file='./playground/data/val.json'
+# python convert/flickr30k.py --input_file='../.cache/lavis/flickr30k/annotations/test.json' --output_file='./playground/data/test.json'
+
+# Copy required annotations to eval dataset
+# TODO eval questions have different format
+# mkdir -p ./playground/data/eval/flickr30k
+# cp ./playground/data/*.json ./playground/data/eval/flickr30k/
+
+# Link image to train and eval dataset
+ln -s ~/.cache/lavis/flickr30k/images/flickr30k-images playground/data/flickr30k-images
+# ln -s ~/.cache/lavis/flickr30k/images/flickr30k-images playground/data/eval/flickr30k/flickr30k-images

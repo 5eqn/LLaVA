12f427bbb1d142fe77724836177ef01314788c16
5eqn <491100866@qq.com>
2024-09-01 20:30:57 +0800

    fine tune from already tuned model

--- scripts/v1_5/finetune-flickr30k.sh
+++ scripts/v1_5/finetune-flickr30k.sh
@@ -2,12 +2,11 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
     --data_path ./playground/data/train.json \
     --image_folder ./playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ../llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \

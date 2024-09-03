f39bd90c2faa399459482fba7608443b252739f2
5eqn <subcat2077@gmail.com>
2024-09-03 10:05:52 +0800

    okvqa eval

--- convert/flickr30k-eval.sh
+++ convert/flickr30k-eval.sh
@@ -1,7 +1,7 @@
 # Ensure eval dataset folder exists
 mkdir -p ./playground/data/eval/flickr30k
 
-# Convert Flickr30k dataset to compatible format for train dataset
+# Convert Flickr30k dataset to compatible format for eval dataset
 python convert/flickr30k-eval.py '~/.cache/lavis/flickr30k/annotations/test.json' './playground/data/eval/flickr30k/test.jsonl'
 
 # Link image to train and eval dataset
---
+++ convert/okvqa-eval.sh
@@ -0,0 +1,8 @@
+# Ensure eval dataset folder exists
+mkdir -p ./playground/data/eval/okvqa
+
+# Convert OKVQA dataset to compatible format for eval dataset
+python convert/okvqa-eval.py ./playground/data/eval/okvqa/OpenEnded_mscoco_val2014_questions.json ./playground/data/eval/okvqa/test.jsonl
+
+# Link image to train and eval dataset
+ln -s ~/.cache/lavis/coco/images/val2014 playground/data/eval/okvqa/val2014

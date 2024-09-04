ee1c71dcfa683e848cc3ce87fafde3b80f1f1efa
5eqn <491100866@qq.com>
2024-09-04 21:36:01 +0800

    fix script

--- convert/okvqa-train.sh
+++ convert/okvqa-train.sh
@@ -2,7 +2,7 @@
 mkdir -p ./playground/data/train/okvqa
 
 # Convert OKVQA dataset to compatible format for train dataset
-python convert/okvqa-eval.py ./playground/data/eval/okvqa/OpenEnded_mscoco_train2014_questions.json ./playground/data/eval/okvqa/mscoco_train2014_annotations.json ./playground/data/train/okvqa/train.json
+python convert/okvqa-train.py ./playground/data/eval/okvqa/OpenEnded_mscoco_train2014_questions.json ./playground/data/eval/okvqa/mscoco_train2014_annotations.json ./playground/data/train/okvqa/train.json
 
 # Link image to train dataset
 ln -s ~/.cache/lavis/coco/images/test2014 ./playground/data/train/okvqa/test2014
